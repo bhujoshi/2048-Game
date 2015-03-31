f7a88b3161652f27ae724651109d23c5867dbb28
unknown <bhuwanchandra.it@gmail.com>
2015-03-31 22:56:29 +0530

    touch events added

--- js/touch_events.js
+++ js/touch_events.js
@@ -1,21 +1,56 @@
 window.addEventListener('load', function(){
-	reload=getElementsByClassName('reload');
-	gameover=getElementById('gameover');
+	
 	var startx = 0;
 	var starty = 0;
-    var dist = 0;
-	gamebody=getElementById('gamebody');
+    var distX = 0;
+    var distY = 0;
 	
-	reload.addEventListener('touchstart', function(e){
-		gameBox();
-		e.preventDefault()
+	
+	document.getElementById('container').addEventListener('touchstart', function(e){
+	  var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
+	  startx = parseInt(touchobj.clientX); // get x position of touch point relative to left edge of browser
+	  starty = parseInt(touchobj.clientY);
+	  //alert(startx+','+starty);
+	  e.preventDefault();
 	}, false);
 	
-	gameover.addEventListener('touchstart', function(e){
-		gameBox();
-		e.preventDefault()
-	}, false);
+	
+	document.getElementById('container').addEventListener('touchmove', function(e){
+		var touchobj = e.changedTouches[0];// reference first touch point (ie: first finger)
+        //startx = parseInt(touchobj.clientX);// get x position of touch point relative to left edge of browser
+		//starty = parseInt(touchobj.clientY);
+		distX = parseInt(touchobj.clientX) - startx;
+		
+		if(distX>=50){
+			swapRight();
+			touchobj=0;
+		}
+		
+		if(distX<=-50){
+			swapLeft();
+			touchobj=0;
+		}
+		
+		e.preventDefault();
+	},false);
 	
 	
+	document.getElementById('container').addEventListener('touchmove', function(e){
+		var touchobj = e.changedTouches[0];
+		distY = parseInt(touchobj.clientY) - starty;
+		
+		if(distY>=50){
+			swapButtom();
+			touchobj=0;
+		}
+		
+		if(distY<=-50){
+			swapUp();
+			touchobj=0;
+		}
+		
+		e.preventDefault();
+	},false);
+
 	
-}
+}, false);
